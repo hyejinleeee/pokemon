2bf72b8e035b91c3211608416f22db86ce823165
hyejin <po852alth@naver.com>
2024-07-05 09:57:01 +0900

    로딩 추가

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -30,6 +30,16 @@ const PokemonList: React.FC = () => {
     fetchData();
   }, [page]);
 
+  if (pokemonData.length === 0)
+    return (
+      <div className="flex items-center justify-center min-h-screen mb-100">
+        <div className="flex space-x-2">
+          <div className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"></div>
+          <div className="w-4 h-4 bg-green-500 rounded-full animate-bounce"></div>
+          <div className="w-4 h-4 bg-red-500 rounded-full animate-bounce"></div>
+        </div>
+      </div>
+    );
   if (error) return <div>{error}</div>;
 
   return (
